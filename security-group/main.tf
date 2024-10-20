5a38f80e81b041cbcb69193a536be23c74c85b01
Junaid-Devops <107377867+Junaid-Devops@users.noreply.github.com>
2024-10-20 15:35:21 +0000

    adding template for SG

---
+++ security-group/main.tf
@@ -0,0 +1,10 @@
+module "web_server_sg" {
+  source              =  "app.terraform.io/Agero/security-group/aws"
+  version             = "4.8.0"
+  name                = "test-module"
+  description         = "Security group for with HTTP ports open within VPC"
+  vpc_id              = "vpc-f01b8f88"
+  ingress_cidr_blocks = ["10.10.0.0/16"]
+  
+}
+
